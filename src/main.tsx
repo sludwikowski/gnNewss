6a95e2c9dedb4623cd55ea17ed940b8dab90bc2f
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-03-22 15:31:03 +0100

    add default `store`

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { Provider } from 'react-redux'
+
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { ThemeProvider, CssBaseline } from '@mui/material'
+
+import { store } from './app/store'
 
 import App from './App'
 
@@ -11,12 +16,12 @@ import theme from './themes/theme'
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <App />
       </Router>
     </ThemeProvider>
-  </React.StrictMode>
+  </Provider>
 )

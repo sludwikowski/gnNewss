60cc1976ff7dc3f3eadbe6e1baa835f1c8dced33
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-03-22 13:58:38 +0100

    `sludwikowski-vite-template` init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className={'m-0 p-0'}>
+      <h1 className={'text-3xl font-bold underline '}>
+        Starter sludwikowski
+      </h1>
+    </div>
+  )
+}
+
+export default App

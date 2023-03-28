ae260cf72ff9f23ef50fed3482aa1c25bd9f1477
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-03-28 23:07:15 +0200

    `formatPolishDateTime` refactored

--- src/helpers/formatPolishDateTime.ts
+++ src/helpers/formatPolishDateTime.ts
@@ -1,12 +1,15 @@
-export function formatPolishDateTime(dateStr: string) {
+/**
+ * Formats a date string in the format used in Polish language.
+ * @param dateStr The date string to format.
+ * @returns The formatted date string.
+ */
+export function formatPolishDateTime(dateStr: string): string {
   const date = new Date(dateStr)
-  const day = date.getDate()
-  const month = date.getMonth() + 1
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const year = date.getFullYear()
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-  const seconds = date.getSeconds()
-  return `${day < 10 ? '0' : ''}${day}.${
-    month < 10 ? '0' : ''
-  }${month}.${year} ${hours}:${minutes}:${seconds}`
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const seconds = date.getSeconds().toString().padStart(2, '0')
+  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`
 }
